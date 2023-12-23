c4d3ef69e7051ba19a73ab689eb035671a9f26a0
cbi-legacy <cbixservice@gmail.com>
2023-12-24 00:34:54 +0100

    testing

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -7,6 +7,8 @@ contract Lottery {
     address[] public winners;
     bool public countdownStatus;
     mapping(address => uint) public winnerCounts;
+    uint public prize;
+    address public winner;
 
     event WinnerPicked(address winner, uint256 index, uint256 prize);
     event Status(string status);
@@ -16,7 +18,7 @@ contract Lottery {
     }
 
     function joinGame() external payable {
-        require(msg.value == 0.005 ether, "Incorrect ether value sent");
+        require(msg.value == 0.05 ether, "Incorrect ether value sent");
         require(players.length < 10, "Maximum number of players reached");
         for (uint256 i = 0; i < players.length; i++) {
             require(players[i] != msg.sender, "You are already a participant in the game.");
@@ -28,14 +30,8 @@ contract Lottery {
         return players;
     }
 
-    function checkCountdown() public {
-        require(!countdownStatus, "Countdown has not completed");
-        countdownStatus = true;
-        string memory status = "done";
-        emit Status(status);
-    }
 
-    function random() private view returns (uint) {
+    function random() internal view returns (uint) {
         return uint(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp, players)));
     }
 
@@ -48,20 +44,40 @@ contract Lottery {
         _;
     }
 
-    function pickWinner() public returns (address) {
-        require(countdownStatus, "Countdown not completed");
-        require(players.length >= 3, "This lottery needs more than 3 players");
-        uint index = random() % players.length;
-        address winner = players[index];
-        winnerCounts[winner]++;
-        uint prize = address(this).balance;
-        payable(winner).transfer(prize);
-        winners.push(winner);
-        emit WinnerPicked(winner, index, prize);
-        startAgain();
-        countdownStatus = false;
-        return winner;
-    }
+
+
+function pickWinner() public returns (address) {
+    require(players.length >= 3, "This lottery needs more than 3 players");
+    uint index = random() % players.length;
+    winner = players[index];
+    winnerCounts[winner]++;
+    prize = address(this).balance;
+
+    // Instead of transferring funds immediately, keep them in the contract
+    // until the winner explicitly claims the prize using the transferPrize function.
+    
+    emit WinnerPicked(winner, index, prize);
+    
+    return winner;
+}
+
+function transferPrize() public {
+    require(msg.sender == winner, "Only the winner can claim the prize");
+    
+    // Ensure that the winner is set before attempting to transfer the prize
+    require(winner != address(0), "Winner address not set");
+
+    // Check if there is a prize to transfer
+    require(prize > 0, "No prize available to transfer");
+
+    // Transfer the prize to the winner
+    payable(winner).transfer(prize);
+
+    // Record the winner and reset for the next round
+    winners.push(winner);
+    startAgain();
+}
+
 
     function getWinCount(address player) public view returns (uint) {
         return winnerCounts[player];
