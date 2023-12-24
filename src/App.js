c009bc7d0b0d42a1105833b261c293af4806d39e
cbi-legacy <cbixservice@gmail.com>
2023-12-24 15:01:27 +0100

    change directory

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './input.css';
+import './App.css';
 import Web3 from 'web3';
 import { useState, useEffect } from 'react';
 import Lottery from './contracts/Lottery.json';
@@ -57,96 +57,111 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const connectToBscTestnet = async () => {
+    // const provider = new Web3.providers.HttpProvider("https://rpc-mumbai.maticvigil.com/");
+    const provider = new Web3(window.ethereum)
+    async function connectToContract() {
       try {
-        const provider = new Web3.providers.HttpProvider('https://data-seed-prebsc-1-s1.binance.org:8545/');
         const web3 = new Web3(provider);
         const networkId = await web3.eth.net.getId();
+        console.log(networkId);
         const deployedNetwork = Lottery.networks;
         const deployedId = deployedNetwork[networkId];
-
+        console.log(deployedNetwork,deployedId)
+  console.log(deployedId.address, " 0x64D6cC4305f1B3a361228bEf9667F6f9919547ef")
         if (deployedId) {
           const contract = new web3.eth.Contract(Lottery.abi, deployedId.address);
+          console.log(contract)
           setState({ web3, contract });
         } else {
-          console.error('Contract not deployed on the specified network.');
+          console.error("Contract not deployed on the specified network.");
         }
       } catch (error) {
-        console.error('Error connecting to BSC testnet:', error);
+        console.error("Error connecting to the contract:", error);
       }
-    };
-
-    connectToBscTestnet();
-  }, []);
-
-  
-  // useEffect(() => {
-  //   const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
-  
-  //   async function template() {
-  //     const web3 = new Web3(provider);
-  //     const network_id = await web3.eth.net.getId();
-  //     // console.log("Network ID:", network_id);
-  //     const deployedNetwork = Lottery.networks;
-  //     const deployedid = deployedNetwork[network_id];
-  //     if (deployedid) {
-  //       const contract = new web3.eth.Contract(Lottery.abi, deployedid.address);
-  //       // console.log("Contract Address:", deployedid.address);
-  //       setState({ web3: web3, contract: contract });
-  //     } else {
-  //       console.error("Contract not deployed on the specified network.");
-  //     }
-  //   }
-  //   provider && template();
-  // }, []);
-
-  useEffect(() => {
-    const handleCountdownTick =  () => {
-      setCountdown((prevCountdown) => {
-        // Decrease the countdown by 1 second
-        const newCountdown = prevCountdown - 1;
-        // console.log(countdowncomplete);
-  // Inside the handleCountdownTick function
-const countStatus = async () => {
-  if (newCountdown === 0) {
-    // If the countdown reaches zero, set the complete status
-    setCountdownCompleteStatus(true);
-    console.log(countdowncomplete);
-
-    const statustx = await state.contract.methods.checkCountdown(countdowncomplete).send({ from: address, gas: 400000 });
-    console.log(statustx);
-    const event = statustx.events.Status;
-    const eventstatus = event.returnValues.status;
-    console.log(eventstatus);
-    if (eventstatus) {
-      pickWinner();
-      // console.log("working")
-      setCountdown(defaultDuration);
-      console.log(countdowncomplete);
     }
-
-    localStorage.setItem('countdown', defaultDuration.toString());
-  } else {
-    localStorage.setItem('countdown', newCountdown.toString());
-  }
-};
-
-countStatus();
-
-        return newCountdown;
-    });
-  };
   
-    const intervalId = setInterval(handleCountdownTick, 1000);
-  
-    // Cleanup the interval when the component unmounts
-    return () => clearInterval(intervalId);
-  }, [state.contract, countdowncomplete, setCountdown, setCountdownCompleteStatus]);
+    // Ensure provider is available before attempting to connect
+    if (provider) {
+      connectToContract();
+    } else {
+      console.error("Web3 provider not available.");
+    }
+  }, [/* dependencies if any */]);
   
   
 
+//   useEffect(() => {
+//     const handleCountdownTick =  () => {
+//       setCountdown((prevCountdown) => {
+//         // Decrease the countdown by 1 second
+//         const newCountdown = prevCountdown - 1;
+//         // console.log(countdowncomplete);
+//   // Inside the handleCountdownTick function
+// const countStatus = async () => {
+//   if (newCountdown === 0) {
+//     // If the countdown reaches zero, set the complete status
+//     setCountdownCompleteStatus(true);
+//     console.log(countdowncomplete);
 
+//     const statustx = await state.contract.methods.checkCountdown(countdowncomplete).send({ from: address, gas: 400000 });
+//     console.log(statustx);
+//     const event = statustx.events.Status;
+//     const eventstatus = event.returnValues.status;
+//     console.log(eventstatus);
+//     if (eventstatus) {
+//       pickWinner();
+//       // console.log("working")
+//       setCountdown(defaultDuration);
+//       console.log(countdowncomplete);
+//     }
 
+//     localStorage.setItem('countdown', defaultDuration.toString());
+//   } else {
+//     localStorage.setItem('countdown', newCountdown.toString());
+//   }
+// };
+
+// countStatus();
+
+//         return newCountdown;
+//     });
+//   };
+  
+//     const intervalId = setInterval(handleCountdownTick, 1000);
+  
+//     // Cleanup the interval when the component unmounts
+//     return () => clearInterval(intervalId);
+//   }, [state.contract, countdowncomplete, setCountdown, setCountdownCompleteStatus]);
+  
+  
+
+// const checkTime =()=> {
+//  const Now = new Date();
+//  const Now2 = new Date( "Fri Dec 22 2023 21:05:17 GMT+0100");
+// const Day = Now.getDay();
+// const Hours = Now.getHours();
+// const Min = Now.getMinutes();
+// const Seconds = Now.getSeconds();
+// const Time = Now.getTime();
+//  console.log(Now,Day,Hours,Min,Seconds,Now2);
+
+//  if (Now === Now2){
+// console.log("working")
+//  }
+// }
+
+// checkTime()
+
+// function pick(){
+// pickWinner();
+// }
+
+// const currentTime = new Date();
+// const oneMinuteLater = new Date(currentTime.getTime() + 2 * 60 *1000);
+// const time = new Date();
+// // console.log(time)
+// const timeDiff = oneMinuteLater - currentTime;
+// setTimeout(pick, timeDiff)
 
   
 
@@ -178,29 +193,33 @@ countStatus();
 
   async function joinGame() {
     const { contract, web3 } = state;
-       try {
+  
+    try {
+      // Ensure the wallet is connected and unlock the account
+    
+    
+  
       if (contract) {
+        // Send the transaction to join the game
         const transaction = await contract.methods.joinGame().send({
           from: address,
-          value: web3.utils.toWei("0.005", "ether"),
-          gas: 3000000,
-       
+          value: web3.utils.toWei("0.05", "ether"),
+          gas: 200000, // Adjust gas limit
         });
-        
-        // Log the transaction details to the console
+  
+       
         console.log(transaction);
+  
+        
         getPlayers();
         getBalance();
-        // Update the UI or perform additional actions as needed
       } else {
         console.error('Contract not initialized. Make sure it is deployed and available.');
       }
     } catch (err) {
-      console.error(err.message);
+      console.error('Error joining game:', err.message);
     }
   }
-  
-
   const pickWinner = async () => {
     try {
   
@@ -235,6 +254,39 @@ countStatus();
 
   };
   
+  const Pickwinner = async () => {
+    try {
+      // Call pickWinner function on the smart contract
+      const result = await state.contract.methods.pickWinner().send({ from: address });
+      console.log('Winner picked:', result);
+
+      // Retrieve winner and prize information
+      const winnerResult = await state.contract.methods.winner().call();
+      setWinnerAddress(winnerResult);
+
+      const prizeResult = await state.contract.methods.prize().call();
+      setWinnerPrize(prizeResult);
+    } catch (error) {
+      console.error('Error picking winner:', error);
+    }
+  };
+
+  const claimPrize = async () => {
+    try {
+      // Call transferPrize function on the smart contract
+      const result = await state.contract.methods.transferPrize().send({ from: address });
+      console.log('Prize claimed:', result);
+
+      // Reset winner and prize information
+      setWinnerAddress(null);
+      setWinnerPrize(0);
+    } catch (error) {
+      console.error('Error claiming prize:', error);
+    }
+  };
+
+  
+
 
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
@@ -244,12 +296,12 @@ countStatus();
 
 
   return (
-    <div className="App min-h-screen flex items-center justify-center bg-gradient-to-b from-purple-600 to-indigo-800">
-  <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
-    <h1 className="text-4xl font-bold mb-6 text-center text-gray-800">🎉 Lottery Game 🎲</h1>
+    <div className="App min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-500 via-purple-500 to-pink-500">
+  <div className="bg-white p-8 rounded shadow-md max-w-md w-full">
+    <h1 className="text-4xl font-bold mb-6 text-center text-gray-800">🎮 Lottery Game 🎉</h1>
     <button
       onClick={address ? disconnectWalletHandler : connectWalletHandler}
-      className="bg-yellow-500 text-white px-6 py-3 rounded-full mb-6 w-full hover:bg-yellow-600 focus:outline-none transition-all"
+      className="bg-yellow-500 text-white px-6 py-3 rounded-full hover:bg-yellow-600 mb-6 focus:outline-none transition-all"
     >
       {address ? 'Disconnect Wallet' : 'Connect Wallet'}
     </button>
@@ -262,36 +314,36 @@ countStatus();
     <button
       onClick={joinGame}
       disabled={!address}
-      className="bg-green-500 text-white px-6 py-3 rounded-full mb-8 w-full hover:bg-green-600 focus:outline-none transition-all"
+      className="mt-6 bg-green-500 text-white px-6 py-3 rounded-full hover:bg-green-600 focus:outline-none transition-all"
     >
       🚀 Enter Lottery
     </button>
-    <div className="text-center">
-      <p className="text-lg text-gray-700 mb-4">Countdown: {formatTime(countdown)}</p>
-      <div className="mb-8">
-        <p className="text-lg text-gray-700">
-          {prize ? `🏆 ${prize} Ether was paid to ${winner}` : 'Loading winner'}
-        </p>
-      </div>
-      <p className="text-lg text-gray-700 mb-4">Pot Balance: {balance}</p>
-      <h1 className="text-2xl font-semibold text-gray-800 mb-4">Past Winners: {pastWinners.length}</h1>
-      <div className="space-y-2">
-        {pastWinners.map((pastWinner, index) => (
-          <p key={index} className="text-md text-gray-700">
-            🌟 {pastWinner}
-          </p>
+      <br />
+      <div>
+        <h1>Players {players.length}</h1>
+        {players.map((player, index) => (
+          <p key={index}>{player}</p>
         ))}
       </div>
+      <p>Countdown: {formatTime(countdown)} </p> 
+      {/* {countdown} */}
+      <div>
+        <div>
+          <p>{prize ? `${prize} Matic was paid to ${winner}` : 'Loading winner'}</p>
+        </div>
+      </div>
+      <p>Pool: {balance}</p>
+      <div>
+        <h1>Winners {pastWinners.length}</h1>
+        {pastWinners.map((pastWinner, index) => (
+          <p key={index}>{pastWinner}</p>
+        ))}
+      </div>
+      
+      <button onClick={Pickwinner}>winner</button><br/>
+      <button onClick={claimPrize}>Claim</button>
     </div>
-    <button
-      onClick={pickWinner}
-      className="mt-8 bg-yellow-500 text-white px-6 py-3 rounded-full w-full hover:bg-yellow-600 focus:outline-none transition-all"
-    >
-      🏅 Pick Winner
-    </button>
-  </div>
-</div>
-
+    </div>
   );
 }
 
